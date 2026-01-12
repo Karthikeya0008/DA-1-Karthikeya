a02bb8c2feedf36fd19a9ea809d87228b0dca121
Dara Karthikeya <dara.karthikeya@outlook.com>
2026-01-12 16:43:37 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+    public static void main(String[] args) {
+        // Displaying Personal Details
+        System.out.println("-------------------------------------------");
+        System.out.println("Student Name:    Dara Karthikeya"); 
+        System.out.println("Register Number: 23MIC0008");
+        System.out.println("-------------------------------------------");
+        
+        // Success Message for Jenkins Integration
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("-------------------------------------------");
+    }
+}
